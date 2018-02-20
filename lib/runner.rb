ac33e87a27c9e333cc5c0c00fb9d5c581cd8b898
Yassine Zenati <yassine.zenati@icloud.com>
2018-02-20 16:50:46 +0100

    Empty-comments

--- lib/runner.rb
+++ lib/runner.rb
@@ -6,6 +6,18 @@ module Codecom
   class Runner
     attr_accessor :configuration
 
+    # Describe here what the method should be used for.
+    # Remember to add use case examples if possible.
+    #
+    # @author Yassine Zenati
+    #
+    # Examples:
+    #
+    #   template_options(line)
+    #   #=> @return Expected returned value
+    #
+    # @param line [Class] Write param definition here.
+    # @return [Class] Describe what the method should return.
     def template_options(line)
       {
         author_name: extract_author_name,
@@ -14,6 +26,18 @@ module Codecom
       }
     end
 
+    # Describe here what the method should be used for.
+    # Remember to add use case examples if possible.
+    #
+    # @author Yassine Zenati
+    #
+    # Examples:
+    #
+    #   extract_method_arguments(line)
+    #   #=> @return Expected returned value
+    #
+    # @param line [Class] Write param definition here.
+    # @return [Class] Describe what the method should return.
     def extract_method_arguments(line)
       method = extract_method_name(line)
 
@@ -24,30 +48,102 @@ module Codecom
       end
     end
 
+    # Describe here what the method should be used for.
+    # Remember to add use case examples if possible.
+    #
+    # @author Yassine Zenati
+    #
+    # Examples:
+    #
+    #   extract_author_name
+    #   #=> @return Expected returned value
+    #
+    # @return [Class] Describe what the method should return.
     def extract_author_name
       `git config user.name`.strip
     end
 
+    # Describe here what the method should be used for.
+    # Remember to add use case examples if possible.
+    #
+    # @author Yassine Zenati
+    #
+    # Examples:
+    #
+    #   extract_git_revision
+    #   #=> @return Expected returned value
+    #
+    # @return [Class] Describe what the method should return.
     def extract_git_revision
       path = File.expand_path('../..', File.dirname(__FILE__))
       `git --git-dir #{path}/.git rev-parse --short HEAD`.strip
     end
 
+    # Describe here what the method should be used for.
+    # Remember to add use case examples if possible.
+    #
+    # @author Yassine Zenati
+    #
+    # Examples:
+    #
+    #   extract_method_name(line)
+    #   #=> @return Expected returned value
+    #
+    # @param line [Class] Write param definition here.
+    # @return [Class] Describe what the method should return.
     def extract_method_name(line)
       line.strip.split('def ')[1]
     end
 
+    # Describe here what the method should be used for.
+    # Remember to add use case examples if possible.
+    #
+    # @author Yassine Zenati
+    #
+    # Examples:
+    #
+    #   extract_method_name_without_arguments(line)
+    #   #=> @return Expected returned value
+    #
+    # @param line [Class] Write param definition here.
+    # @return [Class] Describe what the method should return.
     def extract_method_name_without_arguments(line)
       name = extract_method_name(line)
       name.include?('(') ? name.split('(')[0] : name.split(' ')[0]
     end
 
+    # Describe here what the method should be used for.
+    # Remember to add use case examples if possible.
+    #
+    # @author Yassine Zenati
+    #
+    # Examples:
+    #
+    #   process_line(line, index)
+    #   #=> @return Expected returned value
+    #
+    # @param line [Class] Write param definition here.
+    # @param index [Class] Write param definition here.
+    # @return [Class] Describe what the method should return.
     def process_line(line, index)
       method_name = template_options(line).fetch(:method_name)
       replaced_template = replace_template(template, template_options(line))
       indent_template(replaced_template, line.index('def '))
     end
 
+    # Describe here what the method should be used for.
+    # Remember to add use case examples if possible.
+    #
+    # @author Yassine Zenati
+    #
+    # Examples:
+    #
+    #   replace_template(data, options = {})
+    #   #=> @return Expected returned value
+    #
+    # @param data [Class] Write param definition here.
+    # @param options = {} [Class] Write param definition here.
+    # @return [Class] Describe what the method should return.
     def replace_template(data, options = {})
       data = data.gsub('%{method_name}', options.fetch(:method_name))
       data = data.gsub('%{author_name}', options.fetch(:author_name))
@@ -60,24 +156,84 @@ module Codecom
       data.gsub('# %{params}', params)
     end
 
+    # Describe here what the method should be used for.
+    # Remember to add use case examples if possible.
+    #
+    # @author Yassine Zenati
+    #
+    # Examples:
+    #
+    #   param_template
+    #   #=> @return Expected returned value
+    #
+    # @return [Class] Describe what the method should return.
     def param_template
       "# @param %{param} [Class] Write param definition here."
     end
 
+    # Describe here what the method should be used for.
+    # Remember to add use case examples if possible.
+    #
+    # @author Yassine Zenati
+    #
+    # Examples:
+    #
+    #   indent_template(template, index)
+    #   #=> @return Expected returned value
+    #
+    # @param template [Class] Write param definition here.
+    # @param index [Class] Write param definition here.
+    # @return [Class] Describe what the method should return.
     def indent_template(template, index)
       striped_template(template).map do |slice|
         slice.prepend(' ' * index)
       end.join("\n") + "\n"
     end
 
+    # Describe here what the method should be used for.
+    # Remember to add use case examples if possible.
+    #
+    # @author Yassine Zenati
+    #
+    # Examples:
+    #
+    #   striped_template(template)
+    #   #=> @return Expected returned value
+    #
+    # @param template [Class] Write param definition here.
+    # @return [Class] Describe what the method should return.
     def striped_template(template)
       template.strip.split("\n").map(&:strip)
     end
 
+    # Describe here what the method should be used for.
+    # Remember to add use case examples if possible.
+    #
+    # @author Yassine Zenati
+    #
+    # Examples:
+    #
+    #   initialize
+    #   #=> @return Expected returned value
+    #
+    # @return [Class] Describe what the method should return.
     def initialize
       process_comments
     end
 
+    # Describe here what the method should be used for.
+    # Remember to add use case examples if possible.
+    #
+    # @author Yassine Zenati
+    #
+    # Examples:
+    #
+    #   process_comments(source_path = ".", end_with = "*.rb")
+    #   #=> @return Expected returned value
+    #
+    # @param source_path = "." [Class] Write param definition here.
+    # @param end_with = "*.rb" [Class] Write param definition here.
+    # @return [Class] Describe what the method should return.
     def process_comments(source_path = ".", end_with = "*.rb")
       Dir.glob("./#{source_path}/**/#{end_with}").each do |path|
         comments = []
@@ -113,6 +269,18 @@ module Codecom
       end
     end
 
+    # Describe here what the method should be used for.
+    # Remember to add use case examples if possible.
+    #
+    # @author Yassine Zenati
+    #
+    # Examples:
+    #
+    #   template(template_name = 'template.txt')
+    #   #=> @return Expected returned value
+    #
+    # @param template_name = 'template.txt' [Class] Write param definition here.
+    # @return [Class] Describe what the method should return.
     def template(template_name = 'template.txt')
       File.read([File.dirname(__FILE__), template_name].join('/'))
     end
